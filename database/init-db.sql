588e8e33835103e0412097c31f69fed628c683af
Ramsin Khoshabeh <ramsin102@gmail.com>
2022-02-03 11:43:10 -0800

    More Respect to Andrew

--- database/init-db.sql
+++ database/init-db.sql
@@ -18,5 +18,5 @@ INSERT INTO Actors (first_name, last_name, email, age, created_at) VALUES
   ("Zendaya", "", "zenny@greatestshowwoman.com", 25, CURRENT_TIMESTAMP),
   ("Tom", "Holland", "tom@ironmanrocks.com", 25, CURRENT_TIMESTAMP),
   ("Tobey", "Maguire", "tobey@originalspidey.com", 46, CURRENT_TIMESTAMP),
-  ("Andrew", "Garfield", "andy@garfbarf.com", 38, CURRENT_TIMESTAMP)
+  ("Andrew", "Garfield", "andy@theamazingspidey.com", 38, CURRENT_TIMESTAMP)
 ;
